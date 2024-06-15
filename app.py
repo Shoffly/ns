ca2ec28d220140110f7ccba13c56514e4f874b88
Shoffly <80728185+Shoffly@users.noreply.github.com>
2024-06-16 00:36:50 +0400

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def app():
         try:
             email_sender = 'sunnymoh44@gmail.com'
             email_p = 'qywokurzqrfpcufp'
-            email_to = 'yara.elkassabi@cilantrocafe.net'
+            email_to = 'mohammed09ahmed@gmail.com'
             subject = f'Auto Notification report - {ncamp}'
             body = f'''
            <html>
